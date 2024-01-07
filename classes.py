7d1e28f4bdb6691fbb93396bffc872d680a858c9
Tushar Choudhary <chaudharyt99@gmail.com>
2024-01-06 17:32:01 -0800

    added pydantic classes

--- classes.py
+++ classes.py
@@ -1,6 +1,40 @@
-from typing import List
+from typing import List, Optional
 from pydantic import BaseModel
 
 
-class DataFrame(BaseModel):
+class LoadDataFrame(BaseModel):
     file_paths: List[str]
+
+
+class ChangeColumnNames(BaseModel):
+    from_col_names: List[str]
+    to_col_names: List[str]
+
+
+class AltairPlot(BaseModel):
+    data: str
+    x: Optional[str]
+    y: Optional[str]
+    color: Optional[str]
+    # size: Optional[int]
+
+
+class ScatterPlot(AltairPlot):
+    x: str
+    y: str
+
+
+class LinePlot(AltairPlot):
+    x: str
+    y: str
+
+
+class BubblePlot(AltairPlot):
+    x: str
+    y: str
+
+
+class AreaPlot(AltairPlot):
+    x: str
+    y: str
+    func: str
